ed4a3722299d1b8a41988fc0c680ae7d1266d7c0
Zuzana Kašáková <zuza.kasakova@gmail.com>
2017-10-30 19:34:31 +0100

    Pridavam program na pocitani jednicek

---
+++ pocet_jednicek.py
@@ -0,0 +1,14 @@
+
+def pocet_jednicek(cislo):
+    pocet = 0
+    while cislo > 0:
+        zbytek = cislo % 10
+        if zbytek == 1:
+            pocet = pocet + 1
+        cislo = cislo // 10
+    return pocet
+
+
+cislo = int(input('Zadej cislo: '))
+pocet = pocet_jednicek(cislo)
+print('Pocet jednicek v cisle {} je {}.'.format(cislo, pocet))
